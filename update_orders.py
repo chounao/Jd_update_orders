5db6654b80a821a9b5ce17c4b7b011b762c4857f
chaojiezhou <85343028+chounao@users.noreply.github.com>
2024-04-26 17:28:36 +0800

    Update set_data.txt

--- update_orders.py
+++ update_orders.py
@@ -9,102 +9,110 @@ import time
 
 class update_jd_orders:
     def __init__(self):
-        self.patah = os.getcwd()
+        self.path = os.getcwd()
         self.conn = None  # 初始化数据库连接对象
-        self.session = requests.session()
-
+        self.db_info = None
+        self.shop_name = None
+        self.cookie = None
+        self.day = None
+        self.searchId =None
+        self.jd_token = None
+        self.stat_timestamp = None
+        self.end_timestamp = None
+        self.stat_time = None
     def update_session(self):
-        self.cookie = self.read_txt()[2]
+        self.read_txt()
+        self.session = requests.session()
         headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
                    'Cookie': self.cookie}
+        print(headers)
         self.session.headers.update(headers)
         return self.session
 
-    def get_times(self,day):
-        stat_time = datetime.date.today()-datetime.timedelta(days=day)
-        # print(day)
-        stat_timestamp = int(datetime.datetime(stat_time.year,stat_time.month,stat_time.day).timestamp())
-        end_timestamp = int(time.time())
-        # print(end_timestamp,stat_timestamp)
-        return stat_timestamp,end_timestamp
-
+            
+    def get_times(self):
+        self.read_txt()
+        self.stat_time = datetime.date.today()-datetime.timedelta(days=self.day)
+        self.stat_timestamp = int(datetime.datetime(self.stat_time.year,self.stat_time.month,self.stat_time.day).timestamp())
+        self.end_timestamp = int(time.time())
+        return self.stat_timestamp,self.end_timestamp
+    
     def read_txt(self):
         # 读取txt文件
-        with open(self.patah + '/set_data.txt', 'r', encoding='utf-8', errors='ignore') as file:
+        with open(self.path + '/set_data.txt', 'r', encoding='utf-8', errors='ignore') as file:
             data = file.read()
         # 提取数据
         self.db_info = int(data.split('environment = ')[1].split('\n')[0])
         self.day = int(data.split('day = ')[1].split('\n')[0])
-        self.shopname = data.split("shopname = '")[1].split("'\n")[0]
+        self.shop_name = data.split("shopName = '")[1].split("'\n")[0]
         self.cookie = data.split("cookie = '")[1].split("'\n")[0]
         self.searchId = data.split("searchId = '")[1].split("'\n")[0]
-        self.jd_tokne = data.split("jd_token = '")[1].split("'\n")[0]
-        # self.orderid = [int(x) for x in data.split("order = ")[1].split('\n')[0].split(',')]
-        return self.db_info, self.shopname, self.cookie, self.day, self.searchId,self.jd_tokne
-
-    def get_ordersid(self):
-        txt_data = self.read_txt()
-        time = self.get_times(txt_data[3])
+        self.jd_token = data.split("jd_token = '")[1].split("'\n")[0]
+        print(self.cookie)
+        print(self.jd_token)
+        #print( self.db_info, self.shop_name, self.cookie, self.day, self.searchId, self.jd_token)
+        return self.db_info, self.shop_name, self.cookie, self.day, self.searchId, self.jd_token
+    def get_ordersId(self):
+        self.read_txt()
+        self.get_times()
         h = { 'Content - Type':'application / x - www - form - urlencoded'}
         body = {
             'page': 1,
             'pageSize': 200,
             'shopId': 13000723,
-            'startTime':time[0],
-            'endTime': time[1],
-            'shopName':txt_data[1],
+            'startTime':self.stat_timestamp,
+            'endTime': self.end_timestamp,
+            'shopName':self.shop_name,
             'deviceId':'5IE5BGMNVCQ7DOPMXFED7YS2AKYPYNI755RSA7DGILPI5AKRNSLPZI4XAFKBMVV5HGNBEAHJAKEKIYZO7Z2WEML25A',
             'useTime': 5,
             'referer':'jdportal',
-            'token': txt_data[5]
+            'token': self.jd_token
         }
         url ='https://jdapione.fhd001.com/fhd/jdp/queryIncrementOrderByTime.do'
         r = requests.post(url,body,h)
-        # print(r.json()['data'])
         order_list = []
         for i in r.json()['data']['list']:
             order_list.append(i['orderSn'])
         # print(order_list)
         return order_list
     def get_jd_respons(self):
-        txt_data = self.read_txt()
-
-        self.shopname = txt_data[1]
-        orderid = self.get_ordersid()
-        self.searchId = txt_data[4]
-        self.list_response = []
-        # ,headers,shopName,orderSn
         self.session = self.update_session()
-        get_ueserid = 'https://jdcsone.fhd001.com/info/getUserBaseInfo.do'
+        orderid = self.get_ordersId()
+        self.list_response = []
+        get_ueserId = 'https://jdcsone.fhd001.com/info/getUserBaseInfo.do'
         userid_data = {'searchId': self.searchId, 'searchType': 3}
-        id_body = self.session.post(get_ueserid, userid_data)
-        #print(id_body.json())
+        id_body = self.session.post(get_ueserId, userid_data)
+        print(id_body.text)
+        print(id_body.json())
         userid = id_body.json()['data']['userBase']['userId']
 
-        get_shopid = 'https://jdcsone.fhd001.com/info/getThirdPartyInfo.do'
+
+
+        get_shopId = 'https://jdcsone.fhd001.com/info/getThirdPartyInfo.do'
         shopid_data = {'userId': userid}
-        shop_body = self.session.post(get_shopid, shopid_data)
+        shop_body = self.session.post(get_shopId, shopid_data)
         # print(shop_body.json())
         for i in shop_body.json()['data']:
-            if i['name'] == self.shopname:
-                self.shopid = i['platformKey']
+            if i['name'] == self.shop_name:
+                self.shopId = i['platformKey']
+
 
 
         get_order_info = 'https://jdcsone.fhd001.com/info/getOrderDetailInfo.do'
-        sql_temolate_list = []
+        sql_Temolate_list = []
         for id in orderid:
-            order_info_body = {'userId': userid, 'shopId': self.shopid, 'platform': 'JD',
+            order_info_body = {'userId': userid, 'shopId': self.shopId, 'platform': 'JD',
                                'orderSn': id}
             info_data = self.session.post(get_order_info, order_info_body)
             get_infoData = str(info_data.json()['data']['orderSyncInfo'])
             responseJson = json.loads(get_infoData)['responseJson']
             sql_updata_template = "UPDATE jd_push_01.yd_pop_order SET orderId = {},responsejson = {} WHERE state = '{}'"
             sql_select_template = 'SELECT * FROM jd_cluster_01.jdp_order_part WHERE orderSn = {}'
-            sql_update = sql_updata_template.format(id, json.dumps(responseJson, ensure_ascii=False), str(self.shopname))
+            sql_update = sql_updata_template.format(id, json.dumps(responseJson, ensure_ascii=False), str(self.shop_name))
             sel_select = sql_select_template.format(id)
-            sql_temolate_list.append((sql_update, sel_select))
-        # print(sql_temolate_list)
-        return sql_temolate_list
+            sql_Temolate_list.append((sql_update, sel_select))
+        print(sql_Temolate_list)
+        return sql_Temolate_list
 
 
     def set_host(self):
@@ -132,9 +140,9 @@ class update_jd_orders:
         else:
             print('208数据库不存，进行添加操作')
             mapping = '{}\t{}'.format(local.get('ip'), jd_host.get('do_main'))
-            new_countent = re.sub(local_pattern, mapping, content)
+            new_content = re.sub(local_pattern, mapping, content)
             with open(path, 'w') as f:
-                f.write(new_countent)
+                f.write(new_content)
 
 
 
@@ -147,9 +155,9 @@ class update_jd_orders:
             return e
 
     def select_data(self):
-        if self.set_host():
-            pass
-        environment = self.read_txt()[0]
+        # if self.set_host():
+        #     pass
+        self.read_txt()
         sql_txt=self.get_jd_respons()
         db_info = {
             208: [{'host': '192.168.16.200', 'port': 3307, 'user': 'root', 'passwd': '123456qqq','db': 'jd_push_01', 'charset': 'utf8mb4'},
@@ -158,7 +166,7 @@ class update_jd_orders:
                   {'host': '192.168.16.201', 'port': 3307, 'user': 'root', 'passwd': '123456qqq','db': 'qnjy_cluster_01', 'charset': 'utf8mb4'}]
         }
         for key,values in db_info.items():
-            if environment == key:
+            if self.db_info == key:
                 db_data = values[0]
                 # print(db_data)
 
