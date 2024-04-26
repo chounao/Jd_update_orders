f7e3b744e2fd98c6f44cf8675d246f033f808dd6
chaojiezhou <85343028+chounao@users.noreply.github.com>
2024-04-26 17:15:31 +0800

    Update update_orders.py

--- update_orders.py
+++ update_orders.py
@@ -1,3 +1,5 @@
+import datetime
+import re
 import pymysql
 import os
 import requests
@@ -18,23 +20,57 @@ class update_jd_orders:
         self.session.headers.update(headers)
         return self.session
 
+    def get_times(self,day):
+        stat_time = datetime.date.today()-datetime.timedelta(days=day)
+        # print(day)
+        stat_timestamp = int(datetime.datetime(stat_time.year,stat_time.month,stat_time.day).timestamp())
+        end_timestamp = int(time.time())
+        # print(end_timestamp,stat_timestamp)
+        return stat_timestamp,end_timestamp
+
     def read_txt(self):
         # 读取txt文件
         with open(self.patah + '/set_data.txt', 'r', encoding='utf-8', errors='ignore') as file:
             data = file.read()
         # 提取数据
         self.db_info = int(data.split('environment = ')[1].split('\n')[0])
+        self.day = int(data.split('day = ')[1].split('\n')[0])
         self.shopname = data.split("shopname = '")[1].split("'\n")[0]
         self.cookie = data.split("cookie = '")[1].split("'\n")[0]
         self.searchId = data.split("searchId = '")[1].split("'\n")[0]
-        self.orderid = [int(x) for x in data.split("order = ")[1].split('\n')[0].split(',')]
-        # print(self.db_info, self.shopname, self.cookie, self.orderid,self.searchId)
-        return self.db_info, self.shopname, self.cookie, self.orderid, self.searchId
+        self.jd_tokne = data.split("jd_token = '")[1].split("'\n")[0]
+        # self.orderid = [int(x) for x in data.split("order = ")[1].split('\n')[0].split(',')]
+        return self.db_info, self.shopname, self.cookie, self.day, self.searchId,self.jd_tokne
 
+    def get_ordersid(self):
+        txt_data = self.read_txt()
+        time = self.get_times(txt_data[3])
+        h = { 'Content - Type':'application / x - www - form - urlencoded'}
+        body = {
+            'page': 1,
+            'pageSize': 200,
+            'shopId': 13000723,
+            'startTime':time[0],
+            'endTime': time[1],
+            'shopName':txt_data[1],
+            'deviceId':'5IE5BGMNVCQ7DOPMXFED7YS2AKYPYNI755RSA7DGILPI5AKRNSLPZI4XAFKBMVV5HGNBEAHJAKEKIYZO7Z2WEML25A',
+            'useTime': 5,
+            'referer':'jdportal',
+            'token': txt_data[5]
+        }
+        url ='https://jdapione.fhd001.com/fhd/jdp/queryIncrementOrderByTime.do'
+        r = requests.post(url,body,h)
+        # print(r.json()['data'])
+        order_list = []
+        for i in r.json()['data']['list']:
+            order_list.append(i['orderSn'])
+        # print(order_list)
+        return order_list
     def get_jd_respons(self):
         txt_data = self.read_txt()
+
         self.shopname = txt_data[1]
-        self.orderid = txt_data[3]
+        orderid = self.get_ordersid()
         self.searchId = txt_data[4]
         self.list_response = []
         # ,headers,shopName,orderSn
@@ -42,9 +78,8 @@ class update_jd_orders:
         get_ueserid = 'https://jdcsone.fhd001.com/info/getUserBaseInfo.do'
         userid_data = {'searchId': self.searchId, 'searchType': 3}
         id_body = self.session.post(get_ueserid, userid_data)
-        # print(id_body)
+        #print(id_body.json())
         userid = id_body.json()['data']['userBase']['userId']
-
 
         get_shopid = 'https://jdcsone.fhd001.com/info/getThirdPartyInfo.do'
         shopid_data = {'userId': userid}
@@ -57,9 +92,9 @@ class update_jd_orders:
 
         get_order_info = 'https://jdcsone.fhd001.com/info/getOrderDetailInfo.do'
         sql_temolate_list = []
-        for id in self.orderid:
+        for id in orderid:
             order_info_body = {'userId': userid, 'shopId': self.shopid, 'platform': 'JD',
-                               'orderSn': self.orderid[0]}
+                               'orderSn': id}
             info_data = self.session.post(get_order_info, order_info_body)
             get_infoData = str(info_data.json()['data']['orderSyncInfo'])
             responseJson = json.loads(get_infoData)['responseJson']
@@ -72,6 +107,37 @@ class update_jd_orders:
         return sql_temolate_list
 
 
+    def set_host(self):
+        # 'ect/hosts'  Mac的地址
+        path = 'C:\Windows\System32\drivers\etc\hosts'
+        with open(path,'r')as f:
+            content = f.read()
+        local ={'do_main':'m.local', 'ip' : '192.168.16.200'}
+        jd_host = {'do_main':'jdapione.fhd001.com', 'ip' : '192.168.16.208'}
+        jd_pattern = r'\b{}\b'.format(jd_host.get('do_main'))
+        local_pattern = r'\b{}\b'.format(local.get('do_main'))
+        if re.search(local_pattern,content):
+            print('数据库的host做了')
+            try:
+                if re.search(jd_pattern, content):
+                    print('jd的host在208\n修改jdhost')
+                    mapping_jd = '{}\t{}'.format('39.96.252.227:443', jd_host.get('do_main'))
+                    new_countent = re.sub(jd_pattern, mapping_jd, content)
+                    with open(path, 'w') as f:
+                        f.write(new_countent)
+                else:
+                    print('京东host正常')
+            except ValueError as e:
+                return e
+        else:
+            print('208数据库不存，进行添加操作')
+            mapping = '{}\t{}'.format(local.get('ip'), jd_host.get('do_main'))
+            new_countent = re.sub(local_pattern, mapping, content)
+            with open(path, 'w') as f:
+                f.write(new_countent)
+
+
+
 
     def create_connection(self, host, port, user, passwd, db, charset):
         try:
@@ -81,6 +147,8 @@ class update_jd_orders:
             return e
 
     def select_data(self):
+        if self.set_host():
+            pass
         environment = self.read_txt()[0]
         sql_txt=self.get_jd_respons()
         db_info = {
@@ -101,30 +169,25 @@ class update_jd_orders:
                                                      db=db_data['db'],
                                                      charset=db_data['charset'])
                 count = 0
+                print('********************************************************************\n首次操作会同步所有的订单')
                 for i in sql_txt:
-
-                    print("订单还未更新到数据库\n********************************************************************\nUPDATE\n********************************************************************\n")
                     self.conn.execute(i[0])
-                    print(i[0])
+                    print('已经同步{}条数据\n查询是否同步成功'.format(count+1))
                     try:
-                        print('********************************************************************\nSELECT\n********************************************************************\n')
+                        print('{}'.format(i[1]))
                         self.conn.execute(i[1])
                         result = self.conn.fetchall()
-                        print(i[1])
-
-                        count += 1
+                        count+=1
                         num = 10
                         if not result:
                             for t in range(num):
-
                                 self.conn.execute(i[1])
                                 print('预计查询{}次当前第{}次'.format(num,t+1))
                                 time.sleep(3)
                                 if t+1 == num:
-                                    print("在jd_cluster_01.jdp_order_part依然未找到该订单")
-
+                                    print("已经同步{}次，在jd_cluster_01.jdp_order_part依然未找到该订单".format(num))
                         else:
-                            print('已经插入条：{} '.format(count))
+                            print('同步成功,目前更新{}个订单\n*******************************************************************'.format(count))
                     except ValueError as e:
                             return e
 
